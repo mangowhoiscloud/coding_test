4ee9a071399402bb085d06724954c3fe4fb3d5c6
mangowhoiscloud <ryoo0504@gmail.com>
2025-08-12 04:31:38 +0900

    boj_1043:DSU

---
+++ boj_1043_dsu_class.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DSU {
+private:
+    vector<int> p, rank;
+public:
+    DSU(int n){
+        p.resize(n+1);
+        rank.assign(n+1, 1);
+        iota(p.begin(), p.end(), 0);
+    }
+    int find(int x){ 
+        return p[x]==x ? x : p[x]=find(p[x]); 
+    }
+
+    void unite(int a,int b){
+        a=find(a); b=find(b);
+
+        if(a==b) return;
+        if(rank[a] < rank[b]) swap(a,b);
+
+        p[b]=a; rank[a] += rank[b];
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M; cin >> N >> M;
+    int K; cin >> K;        
+ 
+    vector<int> truth(K);
+    vector<vector<int>> parties(M);
+
+    for(auto& t: truth) cin >> t;
+
+    for(int i=0;i<M;i++){
+        int c; cin >> c; parties[i].resize(c);
+        for(int j=0;j<c;j++) cin >> parties[i][j];
+    }
+
+    DSU dsu(N);
+    for(auto& party: parties){
+        int len = party.size();
+        for(int j=1;j<len;j++) 
+            dsu.unite(party[0], party[j]);
+    }
+
+    unordered_set<int> truthRoot;
+    for(int t: truth) truthRoot.insert(dsu.find(t));
+
+    int ans = 0;
+    for(auto& party: parties){
+        bool hasTruth = false;
+        for(int person: party){
+            if(truthRoot.count(dsu.find(person))){
+                hasTruth = true; break;
+            }
+        }
+        ans += !hasTruth;
+    }
+    cout << ans << "\n";
+    return 0;
+}
--- boj_1325.cpp
+++ boj_1325.cpp
@@ -1,33 +1,29 @@
 #include<bits/stdc++.h>
+#define MAX_N 10004
 
 using namespace std;
 
 int N, M, pub, sub, maximum=0;
 
-vector<bool> visits;
-vector<int> trustScore, answers; 
+bool visits[MAX_N] = {false, };
+int  trustScore[MAX_N] = {0,};
 vector<vector<int>> computers; 
 
 int dfs(int here) {
-    visits.at(here) = true;
+    visits[here] = true;
     int res = 1;
 
-    for(auto row: computers) {
-        for(auto com: row) {
-            res += dfs(com);
-        }
+    for(auto sub: computers.at(here)) {
+        if(visits[sub]) continue;
+        res += dfs(sub);
     }
 
-    visits.at(here) = false;
     return res;
 }
 
 int main() {
     cin >> N >> M;
-
-    visits.assign(N+1, false);
     computers .resize(N+1);
-    trustScore.resize(N+1);
 
     for(int i=0; i<M; i++){
         cin >> sub >> pub;
@@ -35,18 +31,13 @@ int main() {
     }
 
     for(int i=1; i < N+1; i++) {
-        trustScore.at(i) = dfs(i);
-        if(trustScore.at(i) == maximum) {
-            answers.push_back(i);
-        }
-        if(trustScore.at(i) > maximum) {
-            answers.clear();
-            answers.push_back(i);
-        }
+        fill(visits, visits+N+1, 0);
+        trustScore[i] = dfs(i);
+        maximum = max(trustScore[i], maximum);
     }
 
-    for(auto ans: answers) {
-        cout << ans << " ";
+    for(int i=1; i < N+1; i++) {
+        if(trustScore[i] == maximum) cout << i << " ";
     }
 
     return 0;
